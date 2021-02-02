c4747f12b6f5ffec528f3293ccee432734d30b53
zhampel <{ID}+{username}@users.noreply.github.com>
2021-02-02 09:20:22 -0700

    Adds usage info to README, updates local path variable defs, ignores run output info in gitignore

--- birdseye/definitions.py
+++ birdseye/definitions.py
@@ -1,7 +1,10 @@
 import os
 
 # Local location of BirdsEye src directory
-BIRDSEYE_DIR = os.path.dirname(os.path.abspath(__file__))
+BIRDSEYE_SRC_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Local directory containing entire BirdsEye repo
-REPO_DIR = os.path.split(BIRDSEYE_DIR)[0]
+REPO_DIR = os.path.split(BIRDSEYE_SRC_DIR)[0]
+
+# Local directory containing run information
+RUN_DIR = os.path.join(REPO_DIR, 'runs')
