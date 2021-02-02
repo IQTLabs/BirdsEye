37b52fcde9383a1444520aa79e4f5bf810497a2e
zhampel <{ID}+{username}@users.noreply.github.com>
2021-02-02 09:02:56 -0700

    Adds definitions python file for local directory paths

---
+++ src/definitions.py
@@ -0,0 +1,7 @@
+import os
+
+# Local location of BirdsEye src directory
+BIRDSEYE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Local directory containing entire BirdsEye repo
+REPO_DIR = os.path.split(BIRDSEYE_DIR)[0]
