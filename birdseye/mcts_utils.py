6ead95fafb69aac8315bbd644cf202d3abecdb77
zhampel <{ID}+{username}@users.noreply.github.com>
2021-02-26 05:59:59 -0700

    Cleans up plotting in notebook output

--- birdseye/mcts_utils.py
+++ birdseye/mcts_utils.py
@@ -6,6 +6,7 @@ import random
 import numpy as np
 from pfilter import ParticleFilter, systematic_resample
 import matplotlib.pyplot as plt
+import matplotlib.tri as tri
 from IPython.display import clear_output
 
 
@@ -413,32 +414,48 @@ def mcts_trial(sensor, depth, c, plotting=False, num_particles=500, iterations=1
 
 def build_plot(xp=[], belief=[], fig=None, ax=None, time_step=None):
 
-    # Make Subplots
+    fig = plt.figure(figsize=(21, 7))
     plt.tight_layout()
     # Put space between plots
     plt.subplots_adjust(wspace=0.5)
     
     # Particle Plot (Polar)
-    plt.subplot(1, 2, 1, polar=True)
+    ax = fig.add_subplot(1, 3, 1, polar=True)
 
     grid_r, grid_theta = [],[]
     plot_r = [row[0] for row in belief]
     plot_theta = np.array([row[1] for row in belief])*np.pi/180
     plot_x_theta = xp[1]*np.pi/180
     plot_x_r = xp[0]
-
     
-    #plt.figure(1)
     clear_output(wait=True)
-    #plt.figure()
-    plt.polar(plot_theta, plot_r, 'ro')
-    plt.polar(plot_x_theta, plot_x_r, 'bo')
-    plt.ylim(-150,150)
-    plt.title('iteration {}'.format(time_step))
+    
+    ax.plot(plot_theta, plot_r, 'ro')
+    ax.plot(plot_x_theta, plot_x_r, 'bo')
+    ax.set_ylim(-150,150)
+    ax.set_title('iteration {}'.format(time_step), fontsize=16)
+   
+
+    # Particle Plot (Polar) with Interpolation
+    ax = fig.add_subplot(1, 3, 2, polar=True)
+
+    # Create grid values first via histogram.
+    nbins = 10
+    counts, xbins, ybins = np.histogram2d(plot_theta, plot_r, bins=nbins)
+
+    # Make a meshgrid for theta, r values
+    tm, rm = np.meshgrid(xbins[:-1], ybins[:-1])
+
+    # Build contour plot
+    ax.contourf(tm, rm, counts)
+    # True position
+    ax.plot(plot_x_theta, plot_x_r, 'bo')
+    ax.set_ylim(-150,150)
+    ax.set_title('Interpolated Belief'.format(time_step), fontsize=16)
     
     
     # Heatmap Plot (Cartesian)
-    plt.subplot(1, 2, 2)
+    ax = fig.add_subplot(1, 3, 3)
     
     cart  = np.array(list(map(pol2cart, belief[:,0], belief[:,1])))
     x = cart[:,0]
@@ -447,17 +464,8 @@ def build_plot(xp=[], belief=[], fig=None, ax=None, time_step=None):
     extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
     #plt.clf()
-    plt.imshow(heatmap.T, extent=extent, origin='lower')
+    ax.imshow(heatmap.T, extent=extent, origin='lower')
     
-    #ax.clear()
-    #ax.plot(plot_theta, plot_r, 'ro')
-    #ax.plot(plot_x_theta, plot_x_r, 'bo')
     plt.show()
-    #fig.canvas.draw()
-    #fig.clf()
-    #plt.pause(0.0001)
-    #plt.clf()
-    
-
 
 
