9116c7e1e27f74222b8ecdd791c8b82907233315
zhampel <17304045+zhampel@users.noreply.github.com>
2021-03-31 19:55:57 -0600

    Adds doc strings to environment script

--- birdseye/env.py
+++ birdseye/env.py
@@ -16,17 +16,31 @@ class RFEnv(object):
 
     def dynamics(self, particles, control=None, **kwargs):
         """Helper function for particle filter dynamics
+
+        Returns
+        -------
+        array_like
+            Updated particle state information
         """
         return np.array([list(self.state.update_state(p, control)) for p in particles])
 
-    def reset(self):
+    def reset(self, num_particles=500):
         """Reset initial state and particle filter
+
+        Parameters
+        ----------
+        num_particles : integer
+            Number of particles to build particle filter
+
+        Returns
+        -------
+        env_obs : array_like
+            Heatmap distribution of observed particles from reset filter
         """
 
         self.iters = 0
         self.state.state_vars = self.state.init_state()
 
-        num_particles=500
         # Setup particle filter
         self.pf = ParticleFilter(
                         prior_fn=lambda n: np.array([self.sensor.near_state(self.state.state_vars) for i in range(n)]),
@@ -48,6 +62,22 @@ class RFEnv(object):
     def step(self, action_idx):
         """Function to make step based on
            state variables and action index
+
+        Parameters
+        ----------
+        action_idx : integer
+            Index for action to make step
+
+        Returns
+        -------
+        env_obs : array_like
+            Heatmap distribution of observed particles from filter
+        reward : float
+            Reward value for specified action
+        0 : int
+            Placeholder integer value
+        info : dict
+            Dictionary to track step specific values (reward, iteration)
         """
 
         # Get action based on index
@@ -68,16 +98,39 @@ class RFEnv(object):
         info = {'episode':{}}
         info['episode']['l'] = self.iters
         info['episode']['r'] = reward
+
         return (env_obs, reward, 0, info)
 
     def env_observation(self): 
+        """Helper function for environment observation
+
+        Returns
+        -------
+        array_like
+            Heatmap distribution of current observed particles
+        """
         return np.expand_dims(self.particle_heatmap_obs(self.pf.particles), axis=0)
         
     def particle_heatmap_obs(self, belief):
+        """Function to build histogram representing
+           belief distribution in cart coords
 
+        Parameters
+        ----------
+        belief : array_like
+            Belief distribution parameters
+
+        Returns
+        -------
+        heatmap : array_like
+            Histogram of belief state
+        """
+        # Transformation of belief to cartesian coords
         cart  = np.array(list(map(pol2cart, belief[:,0], np.radians(belief[:,1]))))
         x = cart[:,0]
         y = cart[:,1]
+
+        # Build two-dim histogram distribution
         heatmap, xedges, yedges = np.histogram2d(x, y, bins=100)
 
         return heatmap
