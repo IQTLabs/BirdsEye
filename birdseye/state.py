5c72ff001618951aef088606fed721a70aad94f0
zhampel <17304045+zhampel@users.noreply.github.com>
2021-03-31 20:15:10 -0600

    Adds docstrings to state class

--- birdseye/state.py
+++ birdseye/state.py
@@ -38,12 +38,35 @@ class RFState(State):
     
 
     def init_state(self):
+        """Function to initialize a random state
+
+        Returns
+        -------
+        array_like
+            Randomly generated state variable array
+        """
         # state is [range, bearing, relative course, own speed]
         return np.array([random.randint(25,100), random.randint(0,359), random.randint(0,11)*30, 1])
    
 
     # returns reward as a function of range, action, and action penalty or as a function of range only
     def reward_func(self, state, action_idx=None, action_penalty=-.05):
+        """Function to calculate reward based on state and selected action
+
+        Parameters
+        ----------
+        state : array_like
+            List of current state variables
+        action_idx : integer
+            Index for action to make step
+        action_penalty : float
+            Penalty value to reward if action provided
+
+        Returns
+        -------
+        reward_val : float
+            Calculated reward value
+        """
     
         # Set reward to 0/. as default
         reward_val = 0.
@@ -71,7 +94,23 @@ class RFState(State):
 
     # returns new state given last state and action (control)
     def update_state(self, state_vars, control):
+        """Update state based on state and action
+
+        Parameters
+        ----------
+        state_vars : list
+            List of current state variables
+        control : action (tuple)
+            Action tuple
+
+        Returns
+        -------
+        State (array_like)
+            Updated state values array
+        """
+
         TGT_SPD = 1
+        # Get current state vars
         r, theta, crs, spd = state_vars
         spd = control[1]
         
@@ -86,13 +125,15 @@ class RFState(State):
         if crs < 0:
             crs += 360
         crs = crs % 360
-        
+       
+        # Get cartesian coords
         x, y = pol2cart(r, np.radians(theta))
 
+        # Transform changes to coords to cartesian
         dx, dy = pol2cart(TGT_SPD, np.radians(crs))
         pos = [x + dx - spd, y + dy]
 
-        # generate next course given current course
+        # Generate next course given current course
         if random.random() >= self.prob:
             crs += random.choice([-1, 1]) * 30
             crs %= 360
@@ -104,6 +145,7 @@ class RFState(State):
         theta = np.degrees(theta_rad)
         if theta < 0:
             theta += 360
+
         return (r, theta, crs, spd)
 
 
