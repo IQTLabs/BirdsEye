Adds gaussian weighting function in hopes of perf improvement. Corrects radial distance sample in gen_state of SignalStrength

birdseye/sensor.py
@@ -36,7 +36,10 @@ class SignalStrength(Sensor):
     def weight(self, hyp, obs, state): 
         expected_r = state[0]
         obs_r = np.sqrt(1/obs)
-        weight = scipy.stats.norm(expected_r, self.std_dev).pdf(obs_r)[0][0]
+        # Gaussian weighting function
+        numer_fact = np.power(expected_r - obs_r, 2.) 
+        denom_fact = 2 * np.power(self.std_dev, 2.)
+        weight = np.exp( - numer_fact / denom_fact)
         return weight
     
     # samples observation given state
@@ -44,9 +47,9 @@ class SignalStrength(Sensor):
         return 1/ ((np.random.normal(state[0], self.std_dev)) ** 2)
 
     # sample state from observation 
-    def gen_state(self, obs): 
-        r = np.sqrt(1/obs)    
-        return [random.randint(25,100), random.randint(0,359), random.randint(0,11)*30, 1]
+    def gen_state(self, obs):
+        r_dist = np.sqrt(1/obs)    
+        return [r_dist, random.randint(0,359), random.randint(0,11)*30, 1]
 
     def near_state(self, state): 
         return np.array(self.gen_state(self.observation(state)))