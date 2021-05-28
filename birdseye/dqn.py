ff046932ddee4b939afb82d452a6e178dc4bd2ab
Lucas Tindall <ltindall@ucsd.edu>
2021-05-27 17:37:57 -0700

    update dqn, add trials to test result

--- birdseye/dqn.py
+++ birdseye/dqn.py
@@ -262,13 +262,19 @@ def run_dqn(env, config, global_start_time):
         if save_interval and n_iter % save_interval == 0:
             torch.save([qnet.state_dict(), optimizer.state_dict()],
                        os.path.join(save_path, '{}_{}.checkpoint'.format(global_start_time, n_iter)))
-            result = test(env, qnet, max_episode_length, device, ob_scale)
-            result = [datetime.now(), n_iter] + result
+            trials = 100
+            results_list = []
+            for i in range(trials): 
+                result = test(env, qnet, max_episode_length, device, ob_scale)
+                results_list.append(result)
+            result_avg = [np.mean(results_list[:][i]) for i in range(len(results_list[0]))]
+            result = [datetime.now(), n_iter] + result_avg
             run_data.append(result)
 
             filename = '{}/dqn/{}_data.csv'.format(RUN_DIR, global_start_time)
             df = pd.DataFrame(run_data, columns=['time','n_iter','total_reward','collisions','lost', 'reward', 'r_err', 'theta_err', 'heading_err', 'centroid_err', 'rmse'])
             df.to_csv(filename)
+    logger.shutdown()
 
 def test(env, qnet, number_timesteps, device, ob_scale):
     """ Perform one test run """
