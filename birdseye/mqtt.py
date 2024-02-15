8c2c752bbb57c8ee6164245d88353c6ef9cbb080
Lucas Tindall <ltindall@ucsd.edu>
2024-02-14 21:57:16 -0500

    Add MQTT log for all subscribed topics

--- birdseye/mqtt.py
+++ birdseye/mqtt.py
@@ -1,20 +1,26 @@
 import json
 import logging
+import os
 import paho.mqtt.client
 import sys
 
+from pathlib import Path
+
 
 class BirdsEyeMQTT:
-    def __init__(self, mqtt_host, mqtt_port, topics):
+    def __init__(self, mqtt_host, mqtt_port, topics, log_path, start_time):
         self.mqtt_host = mqtt_host
         self.mqtt_port = mqtt_port
         self.topics = topics
+        self.log_path = log_path
+        self.start_time = start_time
+
+        Path(self.log_path).mkdir(parents=True, exist_ok=True)
+
         try:
             self.client = paho.mqtt.client.Client()
             self.client.on_connect = self.on_connect
-            # self.client.on_message = self.on_message
             self.client.on_publish = self.on_publish
-            # self.message_handler = message_handler
             self.client.connect(mqtt_host, mqtt_port, 60)
             self.client.loop_start()
 
@@ -30,9 +36,21 @@ class BirdsEyeMQTT:
     def on_message_func(self, message_handler):
         def on_message(client, userdata, json_message):
             json_data = json.loads(json_message.payload)
+            self.log(json_data)
             message_handler(json_data)
 
         return on_message
+
+    def log(self, json_data):
+        try:
+            with open(
+                os.path.join(self.log_path, f"mqtt-{self.start_time}.log"),
+                "a",
+                encoding="utf-8",
+            ) as f:
+                f.write(f"{json.dumps(json_data)}\n")
+        except FileNotFoundError as err:
+            logging.error(f"could not write to mqtt rssi log: {err}")
 
     def on_connect(self, client, userdata, flags, result_code):
         logging.info(
